cdb5a0e67d6f34239e396095c60bd4423119fe0b
Abdu Zoghbi <a.zoghbi@nasa.gov>
2022-04-14 11:41:42 -0400

    updates to use cloud_access vo column in heasarc/xamin_test

---
+++ first_attempt/run_fornax.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+import astropy.coordinates as coord
+
+import pyvo
+
+sys.path.insert(0, os.getcwd())
+import fornax
+print(f'\nUsing fornax library in: {fornax.__file__}\n')
+
+
+
+# do a simple sia query to chanmaster
+pos = coord.SkyCoord.from_name("ngc 4151")
+query_result = pyvo.dal.sia.search('https://heasarc.gsfc.nasa.gov/xamin_test/vo/sia?table=chanmaster&', pos=pos)
+table_result = query_result.to_table()
+
+
+# data handler
+row = table_result[0]
+
+line = '+'*40
+print(f'\n{line}\nData product:\n{line}')
+
+print(row[['obs_id','target_name','instrument_name', 'access_format']])
+print(f'{line}\n')
+
+
+file = fornax.AWSDataHandler(row).download()
+
