616b3a8286b18e6e1552b659f09771157734e14b
Abdu Zoghbi <a.zoghbi@nasa.gov>
2022-06-21 22:13:40 -0400

    added _summary function

--- run_fornax.py
+++ run_fornax.py
@@ -18,22 +18,17 @@ table_result = query_result.to_table()
 
 
 # data handler
-row_1 = table_result[5]
-row_2 = table_result[6]
+data_product = table_result[5]
 
 line = '+'*40
 print(f'\n{line}\nData product:\n{line}')
 
-print(row_1[['obs_id','target_name','instrument_name', 'access_format']])
-print(row_2[['obs_id','target_name','instrument_name', 'access_format']])
+print(data_product[['obs_id','target_name','instrument_name', 'access_format']])
 print(f'{line}\n')
 
 
 # inject a differnt region name; easier to do here than on the server
 #row_1['cloud_access'] = row_1['cloud_access'].replace('us-east-1', 'us-east-2')
-file = fornax.AWSDataHandler(row_1).download()
-
-
-# inject a differnt bucket name; easier to do here than on the server
-#row_2['cloud_access'] = row_2['cloud_access'].replace('heasarc', 'heasarc2')
-#file = fornax.AWSDataHandler(row_2).download()
+handler = fornax.AWSDataHandler(data_product)
+handler._summary()
+handler.download()
